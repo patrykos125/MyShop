f3b7bd434e3643833221715ff13112a78b2d4209
Mateusz Dziura <tymbark1077@gmail.com>
2023-04-06 23:16:29 +0200

    Dodanie plikow

--- MyShopBackend/src/main/java/com/myshop/model/Order.java
+++ MyShopBackend/src/main/java/com/myshop/model/Order.java
@@ -1,17 +1,34 @@
 package com.myshop.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
+@Entity
 public class Order {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "clinet_id")
+    private Client client;
+
+    @OneToMany
+    @JoinColumn(name = "client_orders_id")
+    private List<Item> items = new ArrayList<>();
+
+
+    @OneToOne
+    private Status status;
+
+    @OneToOne
+    private Shipment shipment;
     private LocalDate date;
-    private Item[] items;
-    private String status;
 }
