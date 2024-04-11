3b6ba7e12898c2917b2692c45a9ac35e350d2ec3
patrykos125 <patrykantoniak1@o2.pl>
2024-04-11 10:27:49 +0200

    update package

--- MyShopBackend/src/main/java/com/myshop/repository/AddressRepository.java
+++ MyShopBackend/src/main/java/com/myshop/repository/AddressRepository.java
@@ -5,9 +5,10 @@ import com.myshop.model.BuyedItems;
 import com.myshop.model.Item;
 import com.myshop.model.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
 }
--- MyShopBackend/src/main/java/com/myshop/repository/BuyedItemsRepository.java
+++ MyShopBackend/src/main/java/com/myshop/repository/BuyedItemsRepository.java
@@ -2,7 +2,9 @@ package com.myshop.repository;
 
 import com.myshop.model.BuyedItems;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BuyedItemsRepository extends JpaRepository<BuyedItems,Long> {
 
 }
